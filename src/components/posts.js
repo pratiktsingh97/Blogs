e8637cc3df352abad3b72ace51e5083d0b06e3e9
pratik_97 <pratiktsingh97@gmail.com>
2019-07-11 00:50:22 +0530

    first commit

---
+++ src/components/posts.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class posts extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+export default posts
